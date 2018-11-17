4d55ffc5e35ed2cfdf277d88f2513dac8c62ba26
Hallgeir Lien <hallgeir.lien@live.com>
2018-11-17 11:33:37 +0100

    fix issue in unit test

--- GrimSearch.Tests/FileUtils/ItemsCacheTests.cs
+++ GrimSearch.Tests/FileUtils/ItemsCacheTests.cs
@@ -14,7 +14,7 @@ namespace GrimSearch.Tests.FileUtils
         {
             ItemCache.Instance.CacheFilename = "Resources\\ItemsCache.json";
 
-            ItemCache.Instance.LoadAllItems(null);
+            ItemCache.Instance.LoadAllItems(null, (msg) => { });
 
             var item = ItemCache.Instance.GetItem("records/items/lootsets/itemset_d017.dbr");
             Assert.AreEqual("records/skills/itemskills/legendary/item_ultoswrath.dbr", item.StringParametersRaw["itemSkillName"]);

9e77d4c2690f0fc01786f9c1f9d27a95deaa8d13
Hallgeir Lien Østerbø <hallgeir.lien@gmail.com>
2023-12-22 05:26:36 +0100

    remove commented code

--- GrimSearch/App.axaml.cs
+++ GrimSearch/App.axaml.cs
@@ -17,7 +17,6 @@ public partial class App : Application
     {
         NLog.LogManager.Setup();
         string assemblyFolder = Path.GetDirectoryName(System.AppContext.BaseDirectory);
-        //NLog.LogManager.Configuration = new NLog.Config.XmlLoggingConfiguration(assemblyFolder + "/NLog.config");
         LogManager.Configuration.Variables["logdirectory"] = ConfigFileHelper.GetConfigFolder();
 
         AvaloniaXamlLoader.Load(this);

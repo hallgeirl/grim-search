27e2182111e723d94efbde63461df5eb64849b4e
Hallgeir Lien Østerbø <hallgeir.lien@gmail.com>
2026-07-30 20:37:05 +0200

    avalonia upgrade

--- GrimSearch/Views/MainWindow.axaml.cs
+++ GrimSearch/Views/MainWindow.axaml.cs
@@ -7,6 +7,7 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Primitives;
 using Avalonia.Input;
+using Avalonia.Input.Platform;
 using Avalonia.Interactivity;
 using Avalonia.Threading;
 using GrimSearch.Common;
